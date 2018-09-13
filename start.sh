ef7d8b1fb0f08b177548e90ef42011254d55a5a6
NithinSethumadhavan <tv.nithin.s@gmail.com>
2018-09-14 02:48:29 +0530

    added openssh-server

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 sudo apt-get upgrade
 sudo apt-get update
-sudo apt-get install vim git terminator zsh git-core
+sudo apt-get install vim git terminator zsh git-core openssh-server
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
